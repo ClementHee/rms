1f77f62d64989ad0f3dc96cb9de392563c3d077b
ClementHee <clement.hee.wy.2001@gmail.com>
2024-03-21 15:37:28 +0800

    Rectify data and UI uniformity

--- resources/views/livewire/maintainence/show_maintainence.blade.php
+++ resources/views/livewire/maintainence/show_maintainence.blade.php
@@ -93,7 +93,7 @@
            
                 <td>
                     <button wire:click="edit({{ $issues->issueNo }})" class="btn btn-primary ">Edit</button>
-                    <button wire:click="deleteConfirm({{ $issues->issueNo }})" class="btn btn-danger mt-1">Delete</button>
+                    <button wire:click="deleteConfirm({{ $issues->issueNo }})" class="btn btn-danger">Delete</button>
                 </td>
 
                
