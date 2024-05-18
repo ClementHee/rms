e2b13f9a1e0da664967ccbfcac8d2a4ddde52221
ClementHee <clement.hee.wy.2001@gmail.com>
2024-05-18 08:56:26 +0800

    Add responsive table to screen

--- app/Http/Livewire/Maintainences.php
+++ app/Http/Livewire/Maintainences.php
@@ -28,11 +28,11 @@ class Maintainences extends Component
  
     
         if($this->filters=='unfixed'){
-            $this->all_maintainence = Maintainence::where('fixed','=',0)->orderBy('reported_at','DESC')->get()->paginate(10);
+            $this->all_maintainence = Maintainence::where('fixed','=',0)->orderBy('reported_at','DESC')->paginate(10);
         }elseif($this->filters=='fixed'){
-            $this->all_maintainence = Maintainence::where('fixed','=',1)->orderBy('reported_at','DESC')->get()->paginate(10);
+            $this->all_maintainence = Maintainence::where('fixed','=',1)->orderBy('reported_at','DESC')->paginate(10);
         }else{
-            $this->all_maintainence = Maintainence::orderBy('reported_at','DESC')->get()->paginate(10);
+            $this->all_maintainence = Maintainence::orderBy('reported_at','DESC')->paginate(10);
         }
 
         return view ('livewire.maintainence.show_maintainence')->layout('livewire.maintainence_dashboard');
