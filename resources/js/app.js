9191748a079cd6c66b41e7891575cf060f47e869
ClementHee <80444883+ClementHee@users.noreply.github.com>
2026-05-12 11:16:01 +0800

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,7 @@ import '../sass/app.scss'
 
 import './../../vendor/power-components/livewire-powergrid/dist/powergrid'
 import './../../vendor/power-components/livewire-powergrid/dist/powergrid.css'
-import './../../vendor/power-components/livewire-powergrid/dist/bootstrap5.css'
+
 import flatpickr from "flatpickr"; 
 import Alpine from 'alpinejs'
 import "flatpickr/dist/flatpickr.min.css";
